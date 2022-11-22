263e2f358c7257bf8a9042d8f2661ae877f49f39
Sangmin Ahn <sanahn@c10r4s6.42seoul.kr>
2022-11-22 18:28:34 +0900

    Add utils functions(ft_chunk_new, ft_token_new, ft_get_last_node, ft_lst_add_back, ft_get_metachar_type)

--- sanahn/main.c
+++ sanahn/main.c
@@ -6,7 +6,7 @@
 /*   By: sanahn <sanahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 13:43:42 by sanahn            #+#    #+#             */
-/*   Updated: 2022/11/22 15:36:49 by yecsong          ###   ########.fr       */
+/*   Updated: 2022/11/22 18:25:34 by sanahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,121 @@
 #include <readline/history.h>
 #include "minishell.h"
 
-void	ft_check_leaks(void)
+t_chunk	*ft_chunk_new(char *content)
 {
-	system("leaks minishell");
+	t_chunk	*res;
+
+	if (content == 0)
+		return (0);
+	res = (t_chunk *)ft_calloc(1, sizeof(t_chunk));
+	if (res == 0)
+		return (0);
+	res->content = content;
+	res->next = 0;
+	return (res);
 }
+
+t_token	*ft_token_new(char *content, int token_type)
+{
+	t_token	*res;
+
+	if (content == 0)
+		return (0);
+	res = (t_token *)ft_calloc(1, sizeof(t_token));
+	if (res == 0)
+		return (0);
+	res->chunks = ft_chunk_new(content);
+	if (res->chunks == 0)
+	{
+		free(res);
+		return (0);
+	}
+	res->type = token_type;
+	res->next = 0;
+	return (res);
+}
+
+void	*ft_get_last_node(void *lst, int data_type)
+{
+	void	*res;
+
+	if (lst == 0)
+		return (0);
+	res = lst;
+	if (data_type == TYPE_CHUNK)
+	{
+		while (((t_chunk *)res)->next)
+			res = ((t_chunk *)res)->next;
+	}
+	else if (data_type == TYPE_TOKEN)
+	{
+		while (((t_token *)res)->next)
+			res = ((t_token *)res)->next;
+	}
+	else
+	{
+		while (((t_cmd *)res)->next)
+			res = ((t_cmd *)res)->next;
+	}
+	return (res);
+}
+
+void	ft_lst_add_back(void **lst, void *new, int data_type)
+{
+	if (lst == 0 || new == 0)
+		return ;
+	if (data_type == TYPE_CHUNK)
+	{
+		if (*((t_chunk **)lst) == 0)
+			*((t_chunk **)lst) = (t_chunk *)new;
+		else
+			((t_chunk *)ft_get_last_node(*lst, data_type))->next \
+				= (t_chunk *)new;
+	}
+	else if (data_type == TYPE_TOKEN)
+	{
+		if (*((t_token **)lst) == 0)
+			*((t_token **)lst) = (t_token *)new;
+		else
+			((t_token *)ft_get_last_node(*lst, data_type))->next \
+				= (t_chunk *)new;
+	}
+	else
+	{
+		if (*((t_cmd **)lst) == 0)
+			*((t_cmd **)lst) = (t_cmd *)new;
+		else
+			((t_cmd *)ft_get_last_node(*lst, data_type))->next = (t_cmd *)new;
+	}
+}
+
+int	ft_get_metachar_type(const char c)
+{
+	if (c == ' ' || c == '\t')
+		return (TYPE_METACHAR_BLANK);
+	if (c == '|' || c == '<' || c == '>')
+		return (TYPE_METACHAR_OPER);
+	return (TYPE_WORD);
+}
+
+// void	ft_tokenize_metachar()
+
+// void	ft_lexer(t_token **tokens, const char *str)
+// {
+
+// }
 
 int	main(void)
 {
 	char	*str;
 	t_token	*tokens;
-	t_cmd	*cmds;
 
-	atexit(ft_check_leaks);
 	tokens = 0;
-	cmds = 0;
-	ft_tokens_init(&tokens);
 	while (42)
 	{
 		str = readline("minishell : ");
 		add_history(str);
-		// 토크나이즈
-		// printf("--------------------------------------------\n");
-		ft_tokenize(&tokens, str);
-		// ft_print_tokens(tokens);
-		// printf("--------------------------------------------\n");
-		// 후처리
-		// printf("--------------------------------------------\n");
-		// 따옴표 처리, 환경 변수 가져오기, 청크 합치기
-		ft_chunk_iteri(tokens, ft_dollar_handler);
-		ft_chunk_iteri(tokens, ft_qutoes_handler);
-		ft_chunk_iteri(tokens, ft_chunk_join);
-		ft_del_empty_token(&tokens);
-		// printf("--------------------------------------------\n");
-		// 파이프를 기준으로 명령줄 나누기
-		ft_set_cmd(&cmds, &tokens);
-		ft_print_cmds(cmds);
 		free(str);
-		ft_cmds_init(&cmds);
-		// system("leaks minishell");
 	}
 	return (0);
 }
--- sanahn/minishell.h
+++ sanahn/minishell.h
@@ -6,25 +6,28 @@
 /*   By: sanahn <sanahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 13:38:56 by sanahn            #+#    #+#             */
-/*   Updated: 2022/11/18 15:32:35 by sanahn           ###   ########.fr       */
+/*   Updated: 2022/11/22 17:41:53 by sanahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# define TYPE_TOKEN_WORD     0
-# define TYPE_TOKEN_OPERATOR 1
-# define TYPE_TOKEN_METACHAR 2
+# define TYPE_CHUNK     0
+# define TYPE_TOKEN     1
+# define TYPE_CMD_TABLE 2
+# define TYPE_CMD       3
 
-# define TYPE_OPER_REDIRECT_IN  0
-# define TYPE_OPER_HEREDOC      1
-# define TYPE_OPER_REDIRECT_OUT 2
-# define TYPE_OPER_APPEND       3
-# define TYPE_OPER_PIPE         4
+# define TYPE_WORD           0
+# define TYPE_METACHAR_BLANK 1
+# define TYPE_METACHAR_OPER  2
+
+# define TYPE_TOKEN_WORD     0
+# define TYPE_TOKEN_METACHAR 1
 
 # include <stdio.h>
 # include <stdlib.h>
+# include "libft/libft.h"
 
 typedef struct s_chunk
 {
@@ -39,49 +42,19 @@ typedef struct s_token
 	struct s_token	*next;
 }	t_token;
 
-typedef struct s_syntax
+typedef struct s_cmd_table
 {
-	t_token	*cmd;
-	t_token	*args;
+	t_token	*command;
+	t_token	*arguments;
 	t_token	*input;
 	t_token	*output;
 	t_token	*error;
-}	t_syntax;
+}	t_cmd_table;
 
 typedef struct s_cmd
 {
-	t_syntax		*syntax;
+	t_cmd_table		*table;
 	struct s_cmd	*next;
 }	t_cmd;
 
-char		*ft_strndup(const char *s1, size_t size);
-void		ft_print_tokens(t_token *tokens);
-void		ft_print_cmds(t_cmd *cmds);
-int			ft_get_operno(t_token *token);
-t_token		*ft_token_pop(t_token **tokens);
-
-t_chunk		*ft_chunk_new(char *content);
-int			ft_chunk_add_back(t_token *token, t_chunk *new);
-t_chunk		*ft_get_last_chunk(t_token *token);
-int			ft_chunk_join(t_chunk *chunk);
-int			ft_chunk_iteri(t_token *tokens, int (*fp)(t_chunk *));
-
-t_token		*ft_token_new(char *content, int type);
-int			ft_token_add_back(t_token **tokens, t_token *new);
-t_token		*ft_get_last_token(t_token *tokens);
-void		ft_token_del(t_token *token);
-void		ft_tokens_init(t_token **tokens);
-
-int			ft_is_metachar(const char c);
-int			ft_get_prev_token_type(const char *str, size_t i);
-int			ft_tokenize_metachar(t_token **tokens, const char *str, size_t *i);
-void		ft_tokenize(t_token **tokens, const char *str);
-
-int			ft_dollar_handler(t_chunk *chunk);
-int			ft_qutoes_handler(t_chunk *chunk);
-int			ft_del_empty_token(t_token **tokens);
-
-t_syntax	*ft_syntax_new(t_token **tokens);
-int			ft_set_cmd(t_cmd **cmds, t_token **tokens);
-void		ft_cmds_init(t_cmd **cmds);
 #endif
