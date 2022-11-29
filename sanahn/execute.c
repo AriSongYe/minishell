0b3ee50c5a24080511df4e2fe5e8c86f344ce0f7
Yechan Song <yecsong@c8r5s6.42seoul.kr>
2022-11-29 19:58:31 +0900

    plus z

--- sanahn/execute.c
+++ sanahn/execute.c
@@ -6,7 +6,7 @@
 /*   By: sanahn <sanahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 16:04:08 by yecsong           #+#    #+#             */
-/*   Updated: 2022/11/29 19:54:02 by yecsong          ###   ########.fr       */
+/*   Updated: 2022/11/29 19:58:20 by yecsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ char	**get_cmd_info(t_cmd **cmd, char **envp);
 int	ft_mystrlen(const char *s)
 {
 	int	i;
+	int	z;
 
 	i = 0;
 	while (s[i] != '\0')
